43f810b7b90aa61ff3b3bf9217f553f2546bed66
sudo97 <i.mihnevich@gmail.com>
2020-08-20 14:05:15 +0300

    do not closure componenets without neccesity

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React from "react";
 
-const Task = ({ task, ...props }) => {
+const ActionBtn = ({ task, doneTask, deleteTask }) => (
+  <div className="btn-bolock">
+    {!task.done ? (
+      <p aria-label="done" onClick={doneTask}>
+        ✔
+      </p>
+    ) : (
+      <p aria-label="delete" onClick={deleteTask}>
+        ✘
+      </p>
+    )}
+  </div>
+);
 
-    const ActionBtn = () => (
-        <div className="btn-bolock">
-            {!task.done ? (
-                <p aria-label="done" onClick={props.doneTask}>✔</p>
-            ) : (
-                <p aria-label="delete" onClick={props.deleteTask}>✘</p>
-            )}
-        </div>
-    );
+const Task = (props) => {
+  const { task } = props;
+  const toggleDone = "task " + (task.done ? "task-done" : "");
 
-    const toggleDone = 'task ' + (task.done ? 'task-done': '');
-
-    return (
-        <div className={toggleDone}>
-            <p>{task.title}</p>
-            <ActionBtn />
-        </div>
-    );
+  return (
+    <div className={toggleDone}>
+      <p>{task.title}</p>
+      <ActionBtn {...props} />
+    </div>
+  );
 };
 
 export default Task;
